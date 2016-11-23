2dc197e5484e5f29a651cac9ef0536950cf56f67
sigurp <skoladoddi@gmail.com>
2016-11-23 19:24:59 +0000

    Lokaverkefni þar sem ég notaði verkefni 6 til að búa til beinagrind af verkefni 3.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # vefforritun_lokaverkefni
 Vinnusvæði fyrir lokaverkefni í vefforritun.
+
+skipanir:
+npm start // til að ræsa síðuna.
+
+npm run lint -s  // til að keyra eslint á viðeigandi kóða.
--- src/routes.js
+++ src/routes.js
@@ -6,21 +6,23 @@ const schedule = require('./schedule');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  const title = 'Sjónvarpsstöðvar';
+  const title = 'Vefforritun - Lokaverkefni';
 
-  schedule.channels()
-    .then((result) => {
-      if (result.data && result.data.results.length > 0) {
-        const channels = result.data.results[0].channels;
-        res.render('index', { channels, title });
-      } else {
-        next(new Error('Úps. Gátum ekki sótt gögnin'));
-      }
-    })
-    .catch((err) => {
-      // console.log(err);
-      next(new Error('Úps. Vitum ekki hvað gerðist'));
-    });
+  res.render('index', { title });
+
+  // schedule.channels()
+  //   .then((result) => {
+  //     if (result.data && result.data.results.length > 0) {
+  //       const channels = result.data.results[0].channels;
+  //       res.render('index', { title });
+  //     } else {
+  //       next(new Error('Úps. Gátum ekki sótt gögnin'));
+  //     }
+  //   })
+  //   .catch((err) => {
+  //     // console.log(err);
+  //     next(new Error('Úps. Vitum ekki hvað gerðist'));
+  //   });
 });
 
 router.get('/tv/:name', (req, res, next) => {
