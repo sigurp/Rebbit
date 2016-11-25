10c4431d1cdbd99c236e8e53b443d168b28826b7
BrynjarSig <brs24@hi.is>
2016-11-25 17:37:51 +0000

    pls

--- routes/threads.js
+++ routes/threads.js
@@ -4,33 +4,6 @@ const pgp = require('pg-promise')();
 
 const db = pgp('postgres://postgres:postgres@localhost/rebbit');
 
-<<<<<<< HEAD
-// threads.get('/threads/:threadID', (req, res, next) => {
-//   console.log('Hú!');
-//   let text, poster;
-// //  res.render('thread', { title: 'req.params.threadID' });
-//   db.one(`select * from threads where id = ${req.params.threadID}`)
-//     .then((thread) => {
-//       text = thread.text;
-//       poster = thread.poster;
-//       console.log('thread', thread);
-//       db.any(`select * from comments where thread = ${req.params.threadID}`)
-//         .then((comments) => {
-//           console.log('Komst í línu 18');
-//           console.log('data', comments);
-//           console.log('Komst í línu 19');
-//           res.render('thread', { poster, text, comments });
-//         })
-//     })
-//     .catch((error) => {
-//       console.log('error', error);
-//       res.render('error', { title: 'Error', 
-//         message: 'Eitthvað fór úrskeiðis!', error });
-//     })
-// });
-
-module.exports = threads;
-=======
 router.get('/threads/:threadID', (req, res, next) => {
   res.render('index', { title: 'req.params.threadID' });
   // db.any('select * from comments where thread = threadID')
@@ -46,4 +19,3 @@ router.get('/threads/:threadID', (req, res, next) => {
 });
 
 module.exports = router;
->>>>>>> c5a41c2ec91ae2796e26bb1ae5b81175c404ce71
