9bd787a0f912c3125af2ccdb5edf8f7cbdfdd362
BrynjarSig <brs24@hi.is>
2016-11-25 11:12:20 +0000

    First commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const pgp = require('pg-promise')();
+
+const db = pgp('postgres://postgres:postgres@localhost/rebbit');
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  db.any('select * from threads')
+    .then((data) => {
+      console.log('data', data);
+      res.render('index', { title: 'Rebbit þræðir', data });
+    })
+    .catch((error) => {
+      console.log('error', error);
+      res.render('error', { title: 'Error', 
+        message: 'Eitthvað fór úrskeiðis!', error });
+    })
+});
+
+
+
+module.exports = router;
