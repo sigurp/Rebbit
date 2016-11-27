b383b8af568317b0c16aa1d0daf0e24ce4565b0f
BrynjarSig <brs24@hi.is>
2016-11-27 22:01:34 +0000

    Posting comments works

---
+++ modules/modules.js
@@ -0,0 +1,36 @@
+
+const pgp = require('pg-promise')();
+
+function getThread(db, req, res) {
+  let text, poster, title;
+  const thrStrID = '\\threads\\' + req.params.threadID;
+
+  db.one(`select * from threads where id = $1`, [req.params.threadID])
+    .then((thread) => {
+      text = thread.text;
+      poster = thread.poster;
+      title = thread.title;
+      console.log('thread', thread);
+      db.any(`select * from comments where thread = $1`,
+        [req.params.threadID])
+        .then((comments) => {
+          console.log('data', comments);
+          res.render('thread', { poster, text, title, comments, thrStrID });
+        })
+    })
+    .catch((error) => {
+      console.log('error', error);
+      res.render('error', { title: 'Error', 
+        message: 'Eitthvað fór úrskeiðis!', error });
+    })
+};
+
+function errorHandler(error) {
+  res.render('error', { title: 'Error',
+    message: 'Eitthvað fór úrskeiðis!', error });
+};
+
+module.exports = {
+  getThread,
+  errorHandler,
+};
--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ const pgp = require('pg-promise')();
 const xss = require('xss');
 
 const router = express.Router();
+const mod = require('../modules/modules');
 
 const DATABASE = process.env.DATABASE_URL || 
   'postgres://postgres:postgres@localhost/rebbit';
@@ -13,7 +14,6 @@ const db = pgp(DATABASE);
 router.get('/', (req, res, next) => {
   db.any('select * from threads')
     .then((data) => {
-      // console.log('get data', data);
       res.render('index', { title: 'Rebbit þræðir', data });
     })
     .catch((error) => {
@@ -43,24 +43,19 @@ router.post('/', (req, res) => {
 });
 
 router.get('/threads/:threadID', (req, res, next) => {
-  let text, poster;
-  db.one(`select * from threads where id = $1`, [req.params.threadID])
-    .then((thread) => {
-      text = thread.text;
-      poster = thread.poster;
-      title = thread.title;
-      console.log('thread', thread);
-      db.any(`select * from comments where thread = $1`,
-        [req.params.threadID])
-        .then((comments) => {
-          console.log('data', comments);
-          res.render('thread', { poster, text, title, comments });
-        })
-    })
-    .catch((error) => {
-      console.log('error', error);
-      res.render('error', { title: 'Error', 
-        message: 'Eitthvað fór úrskeiðis!', error });
+  mod.getThread(db, req, res);
+});
+
+router.post('/threads/:threadID', (req, res) => {
+  let text, poster, title;
+  const name = xss(req.body.comment_name || '');
+  const comment = xss(req.body.comment_text || '');
+
+  console.log('inserting', name, comment);
+  db.none(`insert into comments (name, comment, thread, replyTo, votes)
+    values ($1, $2, $3, $4, $5)`, [name, comment, req.params.threadID, null, 0])
+    .then(() => {
+      mod.getThread(db, req, res)
     })
 });
 
